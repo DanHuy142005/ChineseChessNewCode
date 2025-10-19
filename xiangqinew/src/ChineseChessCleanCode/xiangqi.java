d05f1f9c6190bcc1f344a24eb128eebd4e039e74
DanHuy142005 <nhangu255@gmail.com>
2025-10-19 17:21:36 +0700

    Implement piece movement in xiangqi game
    Added functionality to move a piece on the chessboard.

--- xiangqinew/src/ChineseChessCleanCode/xiangqi.java
+++ xiangqinew/src/ChineseChessCleanCode/xiangqi.java
@@ -3,5 +3,8 @@ public class xiangqi {
 	public static void main(String[] args) {
 		CChessBoard board = new CChessBoard();
 		System.out.println(board);
+		board.movePiece(0, 0, 0, 2);
+		System.out.println(board);
 	}
 }
+
