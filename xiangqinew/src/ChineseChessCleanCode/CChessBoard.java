dd590e2a9a548761b1674d18d5f950d80dd4783b
DanHuy142005 <nhangu255@gmail.com>
2025-12-04 22:36:51 +0700

    Add game over handling and enforce general facing rule

--- xiangqinew/src/ChineseChessCleanCode/CChessBoard.java
+++ xiangqinew/src/ChineseChessCleanCode/CChessBoard.java
@@ -1,20 +1,54 @@
 package ChineseChessCleanCode;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class CChessBoard { 
-	 private boolean isRedTurn = true;
-	final static int ranks = 10;
-    final static int files = 9;
-	private Set<Pieces> pieces = new HashSet<>();
-	 Set<Pieces> getPieces() {
-		    return pieces;
-	 }
-	CChessBoard() {
-	    for (int i = 0; i < 2; i++) {
-	      pieces.add(new Pieces(0 + i * 8, 0, true, Rank.ROOK, "rj"));
-	      pieces.add(new Pieces(1 + i * 6, 0, true, Rank.KNIGHT, "rm"));
+public class CChessBoard {
+        static class Move {
+                int fromCol;
+                int fromRow;
+                int toCol;
+                int toRow;
+
+                Move(int fromCol, int fromRow, int toCol, int toRow) {
+                        this.fromCol = fromCol;
+                        this.fromRow = fromRow;
+                        this.toCol = toCol;
+                        this.toRow = toRow;
+                }
+
+                @Override
+                public String toString() {
+                        return "(" + fromCol + "," + fromRow + ") -> (" + toCol + "," + toRow + ")";
+                }
+        }
+
+        private boolean isRedTurn = true;
+        final static int ranks = 10;
+        final static int files = 9;
+        private Set<Pieces> pieces = new HashSet<>();
+        private boolean gameOver = false;
+        private boolean lastMoverRed = false;
+        private boolean winByNoMoves = false;
+
+        Set<Pieces> getPieces() {
+                return pieces;
+        }
+
+        boolean isRedTurn() {
+                return isRedTurn;
+        }
+
+        boolean isGameOver() {
+                return gameOver;
+        }
+
+        CChessBoard() {
+            for (int i = 0; i < 2; i++) {
+              pieces.add(new Pieces(0 + i * 8, 0, true, Rank.ROOK, "rj"));
+              pieces.add(new Pieces(1 + i * 6, 0, true, Rank.KNIGHT, "rm"));
 	      pieces.add(new Pieces(2 + i * 4, 0, true, Rank.ELEPHENT, "rx"));
 	      pieces.add(new Pieces(3 + i * 2, 0, true, Rank.ADVISOR, "rs"));
 	      pieces.add(new Pieces(1 + i * 6, 2, true, Rank.CANNON, "rp"));
@@ -26,27 +60,57 @@ public class CChessBoard {
 	    }
 	    pieces.add(new Pieces(4, 0, true, Rank.KING, "rb"));
 	    pieces.add(new Pieces(4, 9, false, Rank.KING, "bb"));
-	    for (int i = 0; i < 5; i++) {
-	      pieces.add(new Pieces(i * 2, 3, true, Rank.PAWN, "rz"));
-	      pieces.add(new Pieces(i * 2, 6, false, Rank.PAWN, "bz"));
-	    }
-	  }
-		  Pieces pieceAt(int files, int ranks) {
-		    for (Pieces piece : pieces) {
-		      if (piece.col == files && piece.row == ranks) {
-		        return piece;
-		      }
-		    }
-		    return null;
-		  }
-		  void movePiece(int fromCol, int fromRow, int toCol, int toRow) {
-			  Pieces movingP = pieceAt(fromCol, fromRow);
-			  Pieces targetP = pieceAt(toCol, toRow);
-			  pieces.remove(movingP);
-			  pieces.remove(targetP);
-			  pieces.add(new Pieces(toCol, toRow, movingP.isRed, movingP.rank, movingP.imgName));
-			  isRedTurn = !isRedTurn;
-		  }
+            for (int i = 0; i < 5; i++) {
+              pieces.add(new Pieces(i * 2, 3, true, Rank.PAWN, "rz"));
+              pieces.add(new Pieces(i * 2, 6, false, Rank.PAWN, "bz"));
+            }
+          }
+
+        CChessBoard(CChessBoard other) {
+                this.isRedTurn = other.isRedTurn;
+                for (Pieces p : other.pieces) {
+                        pieces.add(new Pieces(p.col, p.row, p.isRed, p.rank, p.imgName));
+                }
+        }
+
+                  Pieces pieceAt(int files, int ranks) {
+                    for (Pieces piece : pieces) {
+                      if (piece.col == files && piece.row == ranks) {
+                        return piece;
+                      }
+                    }
+                    return null;
+                  }
+                  void movePiece(int fromCol, int fromRow, int toCol, int toRow) {
+                          movePieceInternal(fromCol, fromRow, toCol, toRow, true);
+                  }
+
+                  private void movePieceInternal(int fromCol, int fromRow, int toCol, int toRow, boolean checkGameOver) {
+                          Pieces movingP = pieceAt(fromCol, fromRow);
+                          Pieces targetP = pieceAt(toCol, toRow);
+                          lastMoverRed = movingP != null && movingP.isRed;
+                          pieces.remove(movingP);
+                          pieces.remove(targetP);
+                          pieces.add(new Pieces(toCol, toRow, movingP.isRed, movingP.rank, movingP.imgName));
+                          isRedTurn = !isRedTurn;
+                          if (checkGameOver) {
+                                  updateGameOverState();
+                          }
+                  }
+
+                  private void updateGameOverState() {
+                          boolean redKingAlive = hasKing(true);
+                          boolean blackKingAlive = hasKing(false);
+                          if (!redKingAlive || !blackKingAlive) {
+                                  gameOver = true;
+                                  winByNoMoves = false;
+                                  return;
+                          }
+                          if (getAllValidMoves().isEmpty()) {
+                                  gameOver = true;
+                                  winByNoMoves = true;
+                          }
+                  }
 		  private boolean outBoard(int col, int row) {
 			  return col < 0 || col > 8 || row < 0 || row > 9;
 		  }
@@ -97,14 +161,51 @@ public class CChessBoard {
 			  }
 			  return bd;
 		  }
-		  private boolean selfKilling(int fromCol, int fromRow, int toCol, int toRow, boolean isRed) {
-			  Pieces target = pieceAt(toCol, toRow);
-			  return target != null && target.isRed == isRed;
-		  }
-		  private boolean AdvisorMove(int fromCol, int fromRow, int toCol, int toRow, boolean isRed) {
-			  if (KingAndAdvisoroutPlace(toCol, toRow, isRed)) { 
-				    return false; 
-			  }
+                  private boolean selfKilling(int fromCol, int fromRow, int toCol, int toRow, boolean isRed) {
+                          Pieces target = pieceAt(toCol, toRow);
+                          return target != null && target.isRed == isRed;
+                  }
+
+                  private boolean kingsFacing() {
+                          Pieces redKing = null;
+                          Pieces blackKing = null;
+                          for (Pieces p : pieces) {
+                                  if (p.rank == Rank.KING) {
+                                          if (p.isRed) {
+                                                  redKing = p;
+                                          } else {
+                                                  blackKing = p;
+                                          }
+                                  }
+                          }
+                          if (redKing == null || blackKing == null) {
+                                  return false;
+                          }
+                          if (redKing.col != blackKing.col) {
+                                  return false;
+                          }
+                          int start = Math.min(redKing.row, blackKing.row) + 1;
+                          int end = Math.max(redKing.row, blackKing.row);
+                          for (int r = start; r < end; r++) {
+                                  if (pieceAt(redKing.col, r) != null) {
+                                          return false;
+                                  }
+                          }
+                          return true;
+                  }
+
+                  boolean hasKing(boolean isRed) {
+                          for (Pieces p : pieces) {
+                                  if (p.rank == Rank.KING && p.isRed == isRed) {
+                                          return true;
+                                  }
+                          }
+                          return false;
+                  }
+                  private boolean AdvisorMove(int fromCol, int fromRow, int toCol, int toRow, boolean isRed) {
+                          if (KingAndAdvisoroutPlace(toCol, toRow, isRed)) {
+                                    return false;
+                          }
 			  return isDiagonal(fromCol, fromRow, toCol, toRow) 
 					  && steps(fromCol, fromRow, toCol, toRow) == 1;
 		  }
@@ -141,11 +242,11 @@ public class CChessBoard {
 				  }
 				  return numPiecesBetween(fromCol, fromRow, toCol, toRow) == 1;
 		  }
-		  private boolean PawnMove(int fromCol, int fromRow, int toCol, int toRow, boolean isRed) {
-			  if(steps(fromCol, fromRow, toCol, toRow) != 1) {
-				  return false;
-			  }
-			  if (isRed) {
+                  private boolean PawnMove(int fromCol, int fromRow, int toCol, int toRow, boolean isRed) {
+                          if(steps(fromCol, fromRow, toCol, toRow) != 1) {
+                                  return false;
+                          }
+                          if (isRed) {
 			        if (selfSide(fromRow, true)) {
 			            return (toCol == fromCol && toRow == fromRow + 1);
 			        } else {
@@ -162,19 +263,19 @@ public class CChessBoard {
 			            return false;
 		  }
 			    }
-		  }
-		  boolean validMove(int fromC, int fromR, int toC, int toR) {
-			  if(fromC == toC && fromR == toR || outBoard(toC, toR)) {
-				  return false;
-			  }
-			  Pieces p = pieceAt(fromC, fromR);
+                  }
+                  boolean validMove(int fromC, int fromR, int toC, int toR) {
+                          if(fromC == toC && fromR == toR || outBoard(toC, toR)) {
+                                  return false;
+                          }
+                          Pieces p = pieceAt(fromC, fromR);
 			  if (p == null || p.isRed != isRedTurn || selfKilling(fromC, fromR, toC, toR, p.isRed)) {
 			    return false;
 			  }
 			  boolean ok = false;
-			  switch (p.rank) {
-			    case ADVISOR: 
-			      ok = AdvisorMove(fromC, fromR, toC, toR, p.isRed);
+                          switch (p.rank) {
+                            case ADVISOR:
+                              ok = AdvisorMove(fromC, fromR, toC, toR, p.isRed);
 			      break;
 			    case KING: 
 			      ok = KingMove(fromC, fromR, toC, toR, p.isRed);
@@ -191,14 +292,72 @@ public class CChessBoard {
 			    case CANNON: 
 			      ok = CannonMove(fromC, fromR, toC, toR);
 			      break;
-			    case PAWN: 
-			      ok = PawnMove(fromC, fromR, toC, toR, p.isRed);
-			      break;
-			  }
-			  return ok;
-		  }
-	public String toString() {
-		String board = "";
+                            case PAWN:
+                              ok = PawnMove(fromC, fromR, toC, toR, p.isRed);
+                              break;
+                          }
+                          if (!ok) {
+                                  return false;
+                          }
+                          return !kingFacesAfter(fromC, fromR, toC, toR);
+                  }
+
+        private boolean kingFacesAfter(int fromC, int fromR, int toC, int toR) {
+                CChessBoard hypothetical = new CChessBoard(this);
+                hypothetical.movePieceInternal(fromC, fromR, toC, toR, false);
+                return hypothetical.kingsFacing();
+        }
+
+        Boolean winnerIsRed() {
+                if (!hasKing(true)) {
+                        return false;
+                }
+                if (!hasKing(false)) {
+                        return true;
+                }
+                if (gameOver && winByNoMoves) {
+                        return lastMoverRed;
+                }
+                return null;
+        }
+
+        List<Move> getAllValidMoves() {
+                if (gameOver) {
+                        return new ArrayList<>();
+                }
+                List<Move> moves = new ArrayList<>();
+                for (Pieces p : pieces) {
+                        if (p.isRed != isRedTurn) continue;
+                        for (int c = 0; c < files; c++) {
+                                for (int r = 0; r < ranks; r++) {
+                                        if (validMove(p.col, p.row, c, r)) {
+                                                moves.add(new Move(p.col, p.row, c, r));
+                                        }
+                                }
+                        }
+                }
+                return moves;
+        }
+
+        double materialScore(boolean forRed) {
+                double score = 0;
+                for (Pieces p : pieces) {
+                        double value = 0;
+                        switch (p.rank) {
+                                case KING: value = 1000; break;
+                                case ROOK: value = 9; break;
+                                case CANNON: value = 4.5; break;
+                                case KNIGHT: value = 4; break;
+                                case ELEPHENT: value = 2.5; break;
+                                case ADVISOR: value = 2; break;
+                                case PAWN: value = 1.5; break;
+                        }
+                        score += p.isRed == forRed ? value : -value;
+                }
+                return score;
+        }
+        public String toString() {
+                String board = "";
 		board += " ";
 		for (int i = 0; i < 9; i++) {
 			board += " " + i;
--- xiangqinew/src/ChineseChessCleanCode/xiangqi.java
+++ xiangqinew/src/ChineseChessCleanCode/xiangqi.java
@@ -6,31 +6,62 @@ import java.util.HashSet;
 import java.util.Map;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.Graphics;
+import javax.swing.JOptionPane;
 import java.awt.Image;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 public class xiangqi {
   static Map<String, Image> keyNameValueImage = new HashMap<String, Image>();
-  xiangqi() {
+    xiangqi() {
     CChessBoard brd = new CChessBoard();
     System.out.println(brd);
+    boolean playerIsRed = askPlayerColor();
+    AIDifficulty difficulty = askDifficulty();
     JFrame f = new JFrame("Chinese Chess");
     f.setSize(800, 800);
     f.setLocation(50, 50);
-    f.add(new CChessPanel(brd));
+    f.add(new CChessPanel(brd, true, !playerIsRed, difficulty, playerIsRed));
     f.setVisible(true);
   }
   public static void main(String[] args) throws IOException {
-	  Set<String> imgNames = new HashSet<>(Arrays.asList(
-	          "bj", "bm", "bx", "bs", "bb", "bp", "bz",
+          Set<String> imgNames = new HashSet<>(Arrays.asList(
+                  "bj", "bm", "bx", "bs", "bb", "bp", "bz",
 	          "rj", "rm", "rx", "rs", "rb", "rp", "rz"));
 	    for (String imgName : imgNames) {
-	      File imgFile = new File("./res/" + imgName + ".png");
-	      keyNameValueImage.put(imgName, ImageIO.read(imgFile).getScaledInstance(CChessPanel.side, CChessPanel.side, Image.SCALE_SMOOTH));
+              File imgFile = new File("./res/" + imgName + ".png");
+              keyNameValueImage.put(imgName, ImageIO.read(imgFile).getScaledInstance(CChessPanel.side, CChessPanel.side, Image.SCALE_SMOOTH));
   }
-	    new xiangqi();
+            new xiangqi();
 }
+
+  private AIDifficulty askDifficulty() {
+          Object[] options = {"Elo 800 (Dễ)", "Elo 1200 (Trung bình)", "Elo 1600 (Khó)"};
+          int choice = JOptionPane.showOptionDialog(null,
+                          "Chọn độ khó AI",
+                          "Độ khó",
+                          JOptionPane.DEFAULT_OPTION,
+                          JOptionPane.QUESTION_MESSAGE,
+                          null,
+                          options,
+                          options[1]);
+          switch (choice) {
+                  case 0: return AIDifficulty.EASY;
+                  case 2: return AIDifficulty.HARD;
+                  default: return AIDifficulty.MEDIUM;
+          }
+  }
+
+  private boolean askPlayerColor() {
+          Object[] options = {"Đỏ (đi trước)", "Đen"};
+          int choice = JOptionPane.showOptionDialog(null,
+                          "Bạn muốn chơi quân nào?",
+                          "Chọn màu",
+                          JOptionPane.DEFAULT_OPTION,
+                          JOptionPane.QUESTION_MESSAGE,
+                          null,
+                          options,
+                          options[0]);
+          return choice != 1;
+  }
 }
