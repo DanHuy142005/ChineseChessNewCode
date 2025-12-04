c214f0de03d60ddec9229962402647e5991f3302
DanHuy142005 <nhangu255@gmail.com>
2025-12-04 21:46:35 +0700

    Enhance xiangqi with GUI and image loading
    Refactor xiangqi class to initialize GUI and load images.

--- xiangqinew/src/ChineseChessCleanCode/xiangqi.java
+++ xiangqinew/src/ChineseChessCleanCode/xiangqi.java
@@ -1,10 +1,36 @@
 package ChineseChessCleanCode;
-public class xiangqi {
-	public static void main(String[] args) {
-		CChessBoard board = new CChessBoard();
-		System.out.println(board);
-		board.movePiece(0, 0, 0, 2);
-		System.out.println(board);
-	}
-}
+import java.util.Set;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+public class xiangqi {
+  static Map<String, Image> keyNameValueImage = new HashMap<String, Image>();
+  xiangqi() {
+    CChessBoard brd = new CChessBoard();
+    System.out.println(brd);
+    JFrame f = new JFrame("Chinese Chess");
+    f.setSize(800, 800);
+    f.setLocation(50, 50);
+    f.add(new CChessPanel(brd));
+    f.setVisible(true);
+  }
+  public static void main(String[] args) throws IOException {
+	  Set<String> imgNames = new HashSet<>(Arrays.asList(
+	          "bj", "bm", "bx", "bs", "bb", "bp", "bz",
+	          "rj", "rm", "rx", "rs", "rb", "rp", "rz"));
+	    for (String imgName : imgNames) {
+	      File imgFile = new File("./res/" + imgName + ".png");
+	      keyNameValueImage.put(imgName, ImageIO.read(imgFile).getScaledInstance(CChessPanel.side, CChessPanel.side, Image.SCALE_SMOOTH));
+  }
+	    new xiangqi();
+}
+}
